4393c51ae953dba1b9a29ea55a39494c1bf88438
Edvardas <edvardas.kozlovas@gmail.com>
2026-06-02 20:25:24 +0100

    feat: add bullets

--- internal/game/state.go
+++ internal/game/state.go
@@ -3,12 +3,16 @@ package game
 import "github.com/gdamore/tcell/v3"
 
 type GameState struct {
-	Map  Map
-	Tank Tank
+	Map     Map
+	Tank    Tank
+	Bullets []Bullet
 }
 
 func Render(screen tcell.Screen, state GameState) {
 	RenderMap(screen, state.Map)
 	RenderTank(screen, state.Tank)
+	for _, b := range state.Bullets {
+		RenderBullet(screen, b)
+	}
 	screen.Show()
 }
